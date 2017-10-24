117a2d2a660f725ed4bd2b8184bcd460706ae36d
stefanakraus@gmail.com <stefanakraus@gmail.com>
2017-10-23 21:43:37 -0500

    init lab 5

---
+++ lab5/ruptimeClient.c
@@ -0,0 +1,76 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#define BUFFER_SIZE 256
+#define SERVER_PORT_NUMBER 30542
+#define SERVER_IP = "XXX"
+
+struct sockaddr_in remoteaddr;
+
+int main(int argc, char **argv) {
+
+  char remoteaddr1[32], remoteaddr2[32];
+
+  // INPUT HANDLING
+  if (argc == 3 ){   // Exactly 3 args: invocation, arg1, arg2
+    if (strlen(argv[1]) > 32) {
+      return 1;
+    } else {
+      sscanf(argv[1], "%s",  &remoteaddr1);
+    }
+
+    if (strlen(argv[2]) > 32) {
+      return 1;
+    } else {
+      sscanf(argv[2], "%s",  &remoteaddr2);
+    }
+
+  } else {
+    printf("Error: improper use:\nruptimeClient <remote address 1> <remote address 2>\n");
+    exit(1);
+  }
+
+  int clisock;
+  char buffer[BUFFER_SIZE];
+
+  memset(buffer, 0, sizeof(char) * BUFFER_SIZE);
+
+  // PREPARE CONNECTION
+  if ( ( clisock == socket(PF_INET, SOCK_STREAM, 0) ) < 0) {
+    printf("socket() error\n");
+    exit(1);
+  }
+
+  remoteaddr.sin_family      = PF_INET;
+  remoteaddr.sin_port        = htons(80);
+  remoteaddr.sin_addr.s_addr = inet_addr(SERVER_IP);
+
+  // CONNECT
+  connect(clisock, (sockaddr *), &remoteaddr1, sizeof(remoteaddr1));
+
+  // R / W STREAM
+  if(!read(clisock, buffer, BUFFER_SIZE - 1)) {
+    printf("Error reading from socket (1).");
+  } else {
+    printf("%s\n", buffer);
+  }
+
+  if(!write(clisock, buffer, BUFFER_SIZE - 1)) {
+    printf("Error writing to socket.");
+  } else {
+    printf("%s\n", buffer);
+  }
+
+  if(!read(clisock, buffer, BUFFER_SIZE - 1)) {
+    printf("Error reading from socket (2).");
+  } else {
+    printf("%s\n", buffer);
+  }
+
+  close(clisock);
+
+  return 0;
+}
---
+++ lab5/sak_uptimeClient.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#define BUFSIZE  1024
+// default hostname, port
+#define HOSTNAME "127.0.0.1"
+#define PORT     1337
+
+/*
+* Simple TCP Client.
+* Main flow:
+* 1. Set up the socket
+* 2. Accept input from socket
+* 3. Print and exit.
+* This client is fairly agnostic as to what the server returns as long as
+*  it fits in the buffer. 
+*/
+int main(int argc, char **argv) {
+    int sockfd, port_number, n;
+    struct sockaddr_in serveraddr;
+    struct hostent *server;
+    char *hostname;
+    char buf[BUFSIZE];
+
+    /* check command line arguments */
+    if (argc == 3) {
+      hostname = argv[1];
+      port_number = atoi(argv[2]);
+    } else { // fall back to defaults
+      hostname = HOSTNAME;
+      port_number = PORT;
+    }
+
+    /* create socket */
+    sockfd = socket(PF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0){
+        perror("ERROR opening socket");
+        exit(1);
+      }
+
+    /* gethostbyname: get the server's DNS entry */
+    server = gethostbyname(hostname);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host as %s\n", hostname);
+        exit(1);
+    }
+
+    /* build the server's Internet address */
+    memset((char *) &serveraddr, '\0', sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    strcpy((char *)server->h_addr, (char *)&serveraddr.sin_addr.s_addr);
+    serveraddr.sin_port = htons(port_number);
+
+    /* connect: create a connection with the server */
+    if (connect(sockfd, &serveraddr, sizeof(serveraddr)) < 0){
+      perror("ERROR connecting");
+      exit(1);
+    }
+
+    /* print the server's reply */
+    memset(buf, '\0', BUFSIZE);
+    n = read(sockfd, buf, BUFSIZE);
+    if (n < 0){
+      perror("ERROR reading from socket");
+      exit(1);
+    }
+    printf("%s\n", buf);
+    close(sockfd);
+    return 0;
+}
