10683499ca311647d640336a8ddbf93f7d99f5d1
jsanborn <jsanborn@co2048-05.ece.iastate.edu>
2017-10-26 16:51:59 -0500

    commiting changes from last week

--- lab5/ruptimeServer.c
+++ lab5/ruptimeServer.c
@@ -1,23 +1,21 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <unistd.h>
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <unistd.h>
 #include <signal.h>
-#include <stdio.h>
 #include <string.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <sys/time.h>
-#include <stdlib.h>
 #include <memory.h>
 #include <ifaddrs.h>
 #include <net/if.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 //30542
 
 void main(int argc, char *argv[]){
@@ -27,13 +25,9 @@ void main(int argc, char *argv[]){
 
   int sersock, consock;
   int valread, valwrite;
-  int pid;
-  int test = 50;
-  int pipe[2];
 
 
   int len = sizeof(clientaddr);
-  char *sendToYazan = "Are you happy now? I didn't ask for a protien bar.";
 
   char IP_ADDRESS[16] = "192.168.254.8";
   int logsize = 10;
@@ -41,34 +35,45 @@ void main(int argc, char *argv[]){
 
 
 
-  if( (sersock = socket(PF_INET, SOCK_STREAM, 0)) < 0){
+  if( (sersock = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
     perror("socket() error:\n");
     exit(1);
   }
 
-  serveraddr.sin_family = PF_INET;
+  serveraddr.sin_family = AF_INET;
   serveraddr.sin_port = htons(30542); //ADD port number
-  serveraddr.sin_addr.s_addr = inet_addr(IP_ADDRESS);
-  //serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  //serveraddr.sin_addr.s_addr = inet_addr(IP_ADDRESS);
+  serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-  bind(sersock, ((struct sockaddr *) &serveraddr), sizeof(serveraddr));
+  if(bind(sersock, ((struct sockaddr *) &serveraddr), sizeof(serveraddr)) < 0){
+    perror("bind() error:\n");
+    exit(1);
+  }
 
-  listen(sersock, 10);
+
+  //listen(sersock, 10);
 
   char buffer[256];
-  char *uptime = "/bin/uptime";
+  //char *uptime = "/bin/uptime";
 
   memset(buffer, 0, 256);
 
   while(1){
-    consock = accept(sersock, (struct sockaddr *) &clientaddr, &len);
-    printf("Connection Accepted\n");har *uptime = "/bin/uptime";
-    valread = read(consock, buffer, sizeof(buffer));
+    //consock = accept(sersock, (struct sockaddr *) &clientaddr, &len);
+    //printf("Connection Accepted\n");har *uptime = "/bin/uptime";
+    printf("Waiting...\n");
+    if( valread = recvfrom(sersock, buffer, 256, 0, (struct sockaddr *) &clientaddr, len) < 0){
+      perror("recvfrom() error: \n");
+      exit(1);
+    }
     buffer[valread] = '\0';
     printf("%s\n", buffer);
-    valwrite = write(consock, sendToYazan, strlen(sendToYazan));
-    close(consock);
+    if(sendto(sersock, buffer, valread, 0, (struct sockaddr *) &clientaddr, len) < 0){
+      perror("sendto() error: \n");
+      exit(1);
+    }
   }
 
   close(sersock);
+  return 0;
 }
